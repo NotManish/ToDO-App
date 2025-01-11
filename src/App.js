935887eefe85bf0fd0d70706b3bcec8847888c7a
NotManish <manishshrestha051@gmail.com>
2025-01-11 12:41:36 +0545

    Added checkbox to mark the work as completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
-import DisplayToDo from './Components/DIsplayToDo';
 import InputToDo from './Components/InputToDo';
 
 function App() {
   return (
-          <div className="App">
-              <InputToDo />   
-           </div>
-)
+    <div className="App">
+      <InputToDo />
+    </div>
+  )
 }
 
 export default App;
--- src/Components/DIsplayToDo.js
+++ src/Components/DIsplayToDo.js
@@ -1,27 +1,37 @@
-function DisplayToDo(props){
- 
-  let deleteItem=(index)=>{
-    let filteredList= props.toDoList.filter((element,indx)=>{
-      return indx!==index;
+import { useState } from "react";
+
+
+function DisplayToDo(props) {
+
+  const [checked, setCheck] = useState(false);
+  let handleComplete = () => {
+    setCheck(!checked);
+  }
+
+  let deleteItem = (index) => {
+    let filteredList = props.toDoList.filter((element, indx) => {
+      return indx !== index;
     })
     props.setToDoList(filteredList);
   }
-  
-    return (
-         props.toDoList.map((element,index)=>{
-          
-           return (
-            <ul>
-                <li>
-                {index+1}) {element.charAt(0).toUpperCase()+element.slice(1)}
-                 <input type="checkbox"></input>
-                  <span>
-                    <i className="fa-solid fa-trash-can" onClick={()=>{deleteItem(index);}}></i>
-                    </span>
-                 </li>
-            </ul>
-           )
-         })
-    )
+
+  return (
+    props.toDoList.map((element, index) => {
+
+      return (
+        <ul>
+          <li>
+            {index + 1})
+            {checked ? <p style={{ textDecoration: "line-through" }}>{element.charAt(0).toUpperCase() + element.slice(1)}</p> :
+              <p>{element.charAt(0).toUpperCase() + element.slice(1)}</p>}
+            <input type="checkbox" onClick={handleComplete}></input>
+            <span>
+              <i className="fa-solid fa-trash-can" onClick={() => { deleteItem(index); }}></i>
+            </span>
+          </li>
+        </ul>
+      )
+    })
+  )
 }
 export default DisplayToDo;
