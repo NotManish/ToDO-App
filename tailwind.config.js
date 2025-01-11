13f0f682a0de0027820cd9facb97e082919a5f1a
NotManish <manishshrestha051@gmail.com>
2025-01-11 19:46:00 +0545

    removed traditioal css and install tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}', 
+  ],
+  theme: {
+    extend: {
+      spacing: {
+        '9/10': '90%',
+        '6/10': '60%',
+
+      }
+    },
+  },
+  plugins: [],
+}
